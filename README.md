c6715303ffaee329ed27614aa3a3a5eb5f1e2c65
frialex <fri.b.alex@gmail.com>
2014-12-14 18:36:33 -0500

    Organize view, clean and comment file

--- README.md
+++ README.md
@@ -11,22 +11,23 @@ Goal
 ==========
 
 1. Extract :focus, :hover, :active rules
-2. Apply them to elements that would be the target of the event had user taken action.
+2. Apply them to elements that would be the target of the user action.
 
 
 Why?
 ==========
 
 In order to see the effects of the pseudo rules a user interaction is required.
-This interaction can be in the form of hoving mouse over the element, clicking
+This interaction can be in the form of hovering mouse over the element, clicking
 on the element, Holding the mouse button down on the element.. so on.
 
-Doing this for a multi client website becomes a big job, especially when your
-faced with making style changes to 4-5 different clients: Loading the browser up,
-going to the clients url, taking action, and repeating for all clients.
+Doing this for a multi-client website becomes a big job, especially when your
+making style changes to 4-5 different clients: Loading the browser up,
+going to the clients url, navigating through pages, taking action.
+Repeating for all clients.
 
-Instead, keep the browser windows in a seperate monitor and user live reload.
-Make changes to css file, and observe the changes in the browser without any further action!
+Instead, keep a few browser windows in a seperate monitor and use LiveReload.
+Make changes to css file, and observe the consequence in the browser without any further action!
 
 
 
@@ -35,3 +36,5 @@ High Level Strategy
 
 Starting point
 document.styleSheets contains all the style rules in the web page in a very structured format.
+
+Bringing in a css parser instead of doing indexOf and string concatenation.
--- public/javascripts/StyleGuide.js
+++ public/javascripts/StyleGuide.js
@@ -7,14 +7,35 @@
 //      cssText => selector + rules !! leverage this to find pseudo rules !!
 
 
-$(function(){
+//note: naming functions for a cleaner stack trace
+$(function app_main(){
 console.log('Loading styleguide');
 
-    $.each(document.styleSheets, function(i, ss){
+    //DOM API doesn't provide an easy iterator for objects, options are:
+        //1) for(var key in document.styleSheets){ takeAction(document.stylesheets[key]); }
+        //
+        //2) var fe = Array.prototype.forEach.bind(document.styleSheets);
+        //   fe(function(styleSheet){ takeAction(stylesheet); });
+        //
+        //3) Array.prototype.forEach.call(document.styleSheets, lambda)
+        //
+        //4) Object.keys(document.styleSheets).forEach(lambda)
+        //
+        //5) jQuery
+
+    //Whats the difference between all these methods?
+        //1) Easy check to  .hasOwnProperty() and standard programming construct
+        //2) Since the context is bound to document.styleSheets, you can pass it around
+        //   to lambdas without worrying about execution context
+        //4) Since Object.keys returns an array, you can perform all the functional programming
+        //   methods on it: filter, sort, ... before executing the lambda
+        //   var numeric_keys Object.keys(document.styleSheets).filter(function(k){ return !isNaN(k); });
+
+    $.each(document.styleSheets, function extract_from_browser(i, ss){
         var groupname = ss.href ? ss.href : 'inline-style';
         console.groupCollapsed(groupname);
 
-        $.each(ss.cssRules, function(i, rule){
+        $.each(ss.cssRules, function foreach_browser_rule(i, rule){
             //ignore media queries for now
             if(rule instanceof CSSStyleRule){
                 //multiple pseudos can be part of single ruleset
@@ -37,7 +58,7 @@ console.log('Loading styleguide');
     function extractFocus(rule){
         //console.log('Found :focus');
         //console.log(rule);
-        extractRule(rule, function(sel, ast){
+        extractRule(rule, function extract_focus_cb(sel, ast){
             var dotted = sel.replace(/:focus/g, '.focus');
             setPseudoRuleAsInline(sel, dotted, ast);
         });
@@ -58,10 +79,9 @@ console.log('Loading styleguide');
     //  declarations[]
     //  selectors[]
     function extractRule(rule, callback){
-        //debugger;
         var ast = cssParser(rule.cssText);
-        $.each(ast.stylesheet.rules, function(i, astrule){
-            $.each(astrule.selectors, function(i, sel){
+        $.each(ast.stylesheet.rules, function ast_stylesheet(i, astrule){
+            $.each(astrule.selectors, function ast_rule(i, sel){
                 callback(sel, astrule);
             });
         });
@@ -75,8 +95,8 @@ console.log('Loading styleguide');
             console.log(dotted);
             $all.each(function(i, el){ console.log(el); });
 
-            $.each(astrule.declarations, function(i, d){
-                $all.css(d.property, d.value);
+            $.each(astrule.declarations, function assign_ast_inline(i, d){
+                $all.css(d.property, d.value); //note: primary reason for using an AST
             });
 
             console.groupEnd(sel);
